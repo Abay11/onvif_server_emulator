8627f46d3ccb43d38c5f3fa0a6a72c8d5d2ae87e
alim_abaev <alim.abaev@ru.axxonsoft.com>
2020-08-14 11:37:40 +0300

    migrate to VS 2019

--- build_x64.bat
+++ build_x64.bat
@@ -1,5 +1,6 @@
 cd .\build_x64
 
-cmake ..\ -DCMAKE_TOOLCHAIN_FILE=C:/dev/vcpkg-2020.01/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 15 2017 Win64"
+REM Now here is used hardcoded path to CMake. TODO: fix it
+C:\dev\cmake_3_18\cmake-3.18.1-win64-x64\cmake-3.18.1-win64-x64\bin\CMake.exe ..\ -DCMAKE_TOOLCHAIN_FILE=C:/dev/vcpkg-2020.01/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 16 2019" -A "x64"
 
 pause
