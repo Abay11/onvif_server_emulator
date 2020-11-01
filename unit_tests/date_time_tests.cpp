7c24afca4564dd9fa70f91aeecb0ae0b6dae645e
alim_abaev <alim.abaev@ru.axxonsoft.com>
2020-11-02 00:02:00 +0300

    add utility::datetime

---
+++ unit_tests/date_time_tests.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+
+#include "../utility/DateTime.hpp"
+
+
+
+BOOST_AUTO_TEST_CASE(system_utc_datetime_func)
+{
+	using namespace utility::datetime;
+
+	namespace pt = boost::posix_time;
+	auto actual = posix_to_utc(pt::time_from_string("2020-10-27 11:20:42"));
+	std::string expected = "2020-10-27T11:20:42:000000Z";
+	BOOST_TEST(expected == actual);
+}
