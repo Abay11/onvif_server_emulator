8c2f0c3c9f77ac1746861282b90081c4c6ae74d2
Alim Abaev <alim.abaev@axxonsoft.dev>
2023-07-09 13:14:42 +0300

    feat: add impl of the GetSupportedAnalyticsModulesResponse

--- onvif_services/analytics_service.cpp
+++ onvif_services/analytics_service.cpp
@@ -19,6 +19,7 @@ namespace pt = boost::property_tree;
 
 // a list of implemented methods
 const std::string GetServiceCapabilities = "GetServiceCapabilities";
+const std::string GetSupportedAnalyticsModules = "GetSupportedAnalyticsModules";
 
 namespace osrv
 {
@@ -66,14 +67,99 @@ public:
 	}
 };
 
+struct GetSupportedAnalyticsModulesHandler : public OnvifRequestBase
+{
+private:
+public:
+	GetSupportedAnalyticsModulesHandler(const std::map<std::string, std::string>& xs,
+																			const std::shared_ptr<pt::ptree>& configs)
+			: OnvifRequestBase(GetSupportedAnalyticsModules, auth::SECURITY_LEVELS::READ_MEDIA, xs, configs)
+	{
+	}
+
+	void operator()(std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request) override
+	{
+		// TODO: configuration token in the request is ignored for now
+		auto fillAnalyticsModulesResponse = [](const pt::ptree& configIn, pt::ptree& out) {
+			out.add("tt:AnalyticsModuleContentSchemaLocation",
+							configIn.get<std::string>("AnalyticsModuleContentSchemaLocation"));
+			for (const auto& [key, tree] : configIn.get_child("modules"))
+			{
+				pt::ptree moduleDescr;
+				moduleDescr.add("<xmlattr>.Name", tree.get<std::string>("Name"));
+				moduleDescr.add("<xmlattr>.fixed", tree.get<bool>("fixed"));
+				moduleDescr.add("<xmlattr>.maxInstances", tree.get<std::string>("maxInstances"));
+				for (const auto& [paramsKey, paramsNode] : tree.get_child("Parameters"))
+				{
+					const auto& itemType = paramsNode.get<std::string>("DescriptionType");
+					pt::ptree item;
+					item.add("<xmlattr>.Name", paramsNode.get<std::string>("Name"));
+					item.add("<xmlattr>.Type", paramsNode.get<std::string>("Type"));
+					moduleDescr.add_child("tt:Parameters.tt:" + itemType, item);
+				}
+
+				for (const auto& [messKey, messNode] : tree.get_child("Messages"))
+				{
+					pt::ptree message;
+					message.add("<xmlattr>.IsProperty", messNode.get<bool>("IsProperty"));
+					for (const auto& [sourceKey, sourceNode] : messNode.get_child("Source"))
+					{
+						pt::ptree item;
+						item.add("<xmlattr>.Name", sourceNode.get<std::string>("Name"));
+						item.add("<xmlattr>.Type", sourceNode.get<std::string>("Type"));
+						const auto& itemType = sourceNode.get<std::string>("DescriptionType");
+
+						message.add_child("tt:Source.tt:" + itemType, item);
+					}
+
+					for (const auto& [dataKey, dataNode] : messNode.get_child("Data"))
+					{
+						pt::ptree data;
+						data.add("<xmlattr>.Name", dataNode.get<std::string>("Name"));
+						data.add("<xmlattr>.Type", dataNode.get<std::string>("Type"));
+						const auto& itemType = dataNode.get<std::string>("DescriptionType");
+
+						message.add_child("tt:Data.tt:" + itemType, data);
+					
+					}
+
+					message.add("tt:ParentTopic", messNode.get<std::string>("ParentTopic"));
+
+					moduleDescr.add_child("tt:Messages", message);
+				}
+
+				out.add_child("tt:AnalyticsModuleDescription", moduleDescr);
+			}
+		};
+
+		const auto& modules = service_configs_->get_child("SupportedAnalyticsModules");
+		pt::ptree supportedAnalyticsModules;
+		fillAnalyticsModulesResponse(modules, supportedAnalyticsModules);
+
+		auto envelope_tree = utility::soap::getEnvelopeTree(ns_);
+		envelope_tree.add_child("s:Body.tan:GetSupportedAnalyticsModulesResponse.tan:SupportedAnalyticsModules", supportedAnalyticsModules);
+
+		pt::ptree root_tree;
+		root_tree.put_child("s:Envelope", envelope_tree);
+
+		std::ostringstream os;
+		pt::write_xml(os, root_tree);
+
+		utility::http::fillResponseWithHeaders(*response, os.str());
+	}
+};
+
 } // namespace analytics
 
 AnalyticsService::AnalyticsService(const std::string& service_uri, const std::string& service_name,
 																	 std::shared_ptr<IOnvifServer> srv)
 		: IOnvifService(service_uri, service_name, srv)
 {
+	requestHandlers_.push_back(std::make_shared<analytics::GetServiceCapabilitiesHandler>(
+			xml_namespaces_, configs_ptree_, *srv->DeviceService()->Configs()));
 	requestHandlers_.push_back(
-			std::make_shared<analytics::GetServiceCapabilitiesHandler>(xml_namespaces_, configs_ptree_, *srv->DeviceService()->Configs()));
+			std::make_shared<analytics::GetSupportedAnalyticsModulesHandler>(xml_namespaces_, configs_ptree_));
+	;
 }
 
 } // namespace osrv
